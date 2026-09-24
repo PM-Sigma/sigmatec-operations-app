bdfbd856bc0bd3d683b5896d4c0bbec525237628
PM-Sigma <pm@sigmatec-energy.com>
2026-09-25 00:05:34 +0300

    chore: rebuild (bdi fixes + boot-chunk dedup)

--- ui/sigma-burnsData.js
+++ ui/sigma-burnsData.js
@@ -1,4 +1,4 @@
-import{p as j,r as F,a0 as _,o as x}from"./sigma.js?v=mufxn4rj";import{u as A}from"./sigma-useQuery.js?v=mufxn4rj";import{g as p,s as g}from"./sigma-supabase.js?v=mufxn4rj";import{q as B}from"./sigma-query.js?v=mufxn4rj";/**
+import{p as j,r as F,a0 as _,o as x}from"./sigma.js?v=mug0o872";import{u as A}from"./sigma-useQuery.js?v=mug0o872";import{g as p,s as g}from"./sigma-supabase.js?v=mug0o872";import{q as B}from"./sigma-query.js?v=mug0o872";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- ui/sigma-chevron-up.js
+++ ui/sigma-chevron-up.js
@@ -1,4 +1,4 @@
-import{p as o}from"./sigma.js?v=mufxn4rj";/**
+import{p as o}from"./sigma.js?v=mug0o872";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
